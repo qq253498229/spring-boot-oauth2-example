b1c68f4fd5adc7ce3498355063ec5fa4e374b6dc
王斌 <253498229@qq.com>
2022-03-30 17:48:06 +0800

    review

--- sql/schema.sql
+++ sql/schema.sql
@@ -45,9 +45,9 @@ create table r_role_resource
 create table t_user
 (
     id       int auto_increment primary key,
-    username varchar(100)     not null comment '用户名',
-    password varchar(100)     not null comment '密码，根据需求加密后存储',
-    enable   bit default b'1' not null comment '是否可用，1为可用，0为不可用，默认1可用',
+    username varchar(100)  not null comment '用户名',
+    password varchar(100)  not null comment '密码，根据需求加密后存储',
+    enable   bit default 1 not null comment '是否可用，1为可用，0为不可用，默认1可用',
     constraint t_user_username_uindex unique (username)
 ) comment '用户表';
 
