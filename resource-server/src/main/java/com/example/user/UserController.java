82d7c4873d4e1750bfda8db7ff5f0b97e00e7001
王斌 <253498229@qq.com>
2022-03-30 21:11:17 +0800

    review

--- resource-server/src/main/java/com/example/user/UserController.java
+++ resource-server/src/main/java/com/example/user/UserController.java
@@ -2,13 +2,14 @@ package com.example.user;
 
 import com.example.user.vo.UserVO;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
     @Resource
     UserService userService;
@@ -16,7 +17,7 @@ public class UserController {
     /**
      * 查看用户列表
      */
-    @GetMapping
+    @GetMapping("/user")
     @PreAuthorize("hasAuthority('showUserList')")
     public List<UserVO> userList() {
         return userService.userList();
@@ -25,7 +26,7 @@ public class UserController {
     /**
      * 查看用户详情
      */
-    @GetMapping("/details/{id}")
+    @GetMapping("/user/details/{id}")
     @PreAuthorize("hasAuthority('showUserDetail')")
     public UserVO showUserDetail(@PathVariable Integer id) {
         return userService.showUserDetail(id);
