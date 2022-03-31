da8309d476bd11cb63500d027be66f4f6351cdc1
王斌 <253498229@qq.com>
2022-03-31 10:34:59 +0800

    添加重置用户密码测试用例

--- resource-server/src/test/java/com/example/user/UserProcessTest.java
+++ resource-server/src/test/java/com/example/user/UserProcessTest.java
@@ -1,10 +1,16 @@
 package com.example.user;
 
+import cn.hutool.json.JSONUtil;
 import com.example.config.TestResourceConfig;
+import com.example.generator.mapper.UserMapper;
+import com.example.generator.model.User;
+import com.example.user.vo.ResetUserPasswordVO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,10 +18,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import javax.annotation.Resource;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * 用户接口测试
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("unit-test")
@@ -23,7 +35,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UserProcessTest {
     @Resource
     MockMvc mockMvc;
+    @Resource
+    UserMapper userMapper;
+    @Resource
+    PasswordEncoder passwordEncoder;
 
+    /**
+     * {@link UserController#userList()}
+     */
     @Test
     @WithMockUser(authorities = {"showUserList"})
     void userList() throws Exception {
@@ -48,5 +67,57 @@ public class UserProcessTest {
                 .andExpect(jsonPath("$[1].username").value("admin"))
                 .andDo(print())
         ;
+    }
+
+    /**
+     * {@link UserController#showUserDetail(Integer)}
+     */
+    @Test
+    @WithMockUser(authorities = {"showUserDetail"})
+    void showUserDetail() throws Exception {
+        mockMvc.perform(get("/user/details/{id}", 1))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.name").value("测试用户"))
+                .andExpect(jsonPath("$.age").value(12))
+                .andExpect(jsonPath("$.email").value("test@test.com"))
+                .andExpect(jsonPath("$.gender").value(1))
+                .andExpect(jsonPath("$.username").value("user"))
+        ;
+        mockMvc.perform(get("/user/details/{id}", 2))
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$").isMap())
+                .andExpect(jsonPath("$.id").value(2))
+                .andExpect(jsonPath("$.userId").value(2))
+                .andExpect(jsonPath("$.name").value("管理员用户"))
+                .andExpect(jsonPath("$.age").value(66))
+                .andExpect(jsonPath("$.email").value("admin@test.com"))
+                .andExpect(jsonPath("$.gender").value(2))
+                .andExpect(jsonPath("$.username").value("admin"))
+        ;
+    }
+
+    /**
+     * {@link UserController#resetUserPassword(ResetUserPasswordVO)}
+     */
+    @Test
+    @WithMockUser(authorities = {"resetUserPassword"})
+    void resetUserPassword() throws Exception {
+        User user = userMapper.selectByPrimaryKey(1);
+        assertTrue(passwordEncoder.matches("user", user.getPassword()));
+        String content = JSONUtil.createObj()
+                .set("username", "user")
+                .set("password", "user1")
+                .toString();
+        mockMvc.perform(post("/user/resetUserPassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+                ).andExpect(status().isOk())
+                .andDo(print())
+        ;
+        User user1 = userMapper.selectByPrimaryKey(1);
+        assertTrue(passwordEncoder.matches("user1", user1.getPassword()));
     }
 }
