e672ddae41f0235878db36543e69674580229169
王斌 <253498229@qq.com>
2022-03-31 12:27:33 +0800

    review

--- single-service/service/src/main/java/com/example/generator/mapper/ClientMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.generator.mapper;
-
-import com.example.generator.model.Client;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ClientMapper extends Mapper<Client> {
-}
--- single-service/service/src/main/java/com/example/generator/mapper/RoleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.generator.mapper;
-
-import com.example.generator.model.Role;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface RoleMapper extends Mapper<Role> {
-}
